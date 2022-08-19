cc842106a0be72047daf496850576f772162c35b
Gagan Sood <gagansood777@gmail.com>
2022-08-19 18:23:47 +0530

    laravel restful api's

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+        
+        return Post::create([
+            'title' => $fields['title'],
+            'content' => $fields['content']
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+        $post = Post::find($id);
+        
+        if (!$post) {
+            return response(["message" => "Post doesn't exist"],405);
+        }
+
+        $post->update($request->all());
+
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if (Post::destroy($id)) {
+            $response = ["message", "Post deleted successfully."];
+            $statusCode = 200;
+        } else {
+            $response = ["message", "Post doesn't exist"];
+            $statusCode = 405;
+        }
+        return response($response, $statusCode);
+    }
+}
